000b49b55e9a71f8f07b96adc7756c6940bf0fd5
Fridberg <fridberg.traustason@gmail.com>
2016-09-16 01:37:29 +0000

    Basic backend finished

---
+++ app/database.js
@@ -0,0 +1,18 @@
+var db = require('./util/db');
+
+db.addCollection('top').insert([
+    {term:"JavaScript", style: 'warning'},
+    {term:"Angular 2", style: 'dangerous'},
+    {term:"NodeJS", style: 'success'},
+    {term:"Golang", style: 'info'},
+    {term:"iOS", style: 'default'},
+    {term:"ReactJS", style: 'warning'},
+    {term:"Ionic", style: 'info'},
+    {term:"REST", style: 'primary'},
+    {term:"Authentication", style: 'default'},
+    {term:"Android", style: 'success'},
+]);
+
+db.addCollection('searches');
+
+db.saveDatabase();
