97df03037aba446883d465283a8ff2114f8c565c
Fridberg <fridberg.traustason@gmail.com>
2016-09-17 22:20:33 +0000

    Finish basic twitter project

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '../public/views');
+app.set('views', __dirname + '/../public/views');
 
 app.use(express.static(__dirname + '/../public'));
 
