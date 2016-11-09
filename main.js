339b60ce5a6655dd83d29c73ed2c766646e1cf83
Fridberg <fridberg.traustason@gmail.com>
2016-11-09 17:51:07 +0000

    socket.io add to grengilBot

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ var app = require('./app/server'); //import the contents of servers module.expor
 
 app.set('port', (process.env.PORT || 5000)); //Set the port to either the .env variable or 5000
 
-
 app.listen(app.get('port'), function(){
   console.log('server is running'); //Listen on the port and log success
 });
